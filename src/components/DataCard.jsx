2b570887e8992e39d28d4559a7233a91fb605844
mlarsen <174657206+mlarsen-source@users.noreply.github.com>
2025-10-28 17:35:56 -0700

    modified

--- src/components/DataCard.jsx
+++ src/components/DataCard.jsx
@@ -1,17 +1,27 @@
 export default function DataCard({data}) {
   
   let name = data.name.toUpperCase()
+  let baseExp = data.base_experience
   
   let allTypes = data.types.map((each, index)=>
-    <div key={index}>
-      <p>Slot {each.slot}: {each.type.name}</p>
-    </div>
+    <li key={index}>
+      Slot {each.slot}: {each.type.name}
+    </li>
+  )
+  let allAbilities = data.abilities.map((each, index)=>
+    <li key={index}>
+      Slot {each.slot}: {each.ability.name}
+    </li>
   )
 
   return(
     <section className="data-card-section">
       <h3>{ name }</h3>
-      <div>{ allTypes }</div>
+      <p>Base Exp: {baseExp}</p>
+      <h4>Types: </h4>
+        <ul> {allTypes }</ul>
+      <h4>Abilities:</h4>
+        <ul>{ allAbilities }</ul>
     </section>
   )
 
